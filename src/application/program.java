65b26d0e415d04fe90fc74630acf0682001780ac
Igor Castanheira <igor.castanheira.dev@gmail.com>
2021-04-08 21:10:45 -0300

    Adicionado try catch

--- src/application/program.java
+++ src/application/program.java
@@ -5,20 +5,41 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+import moldel.entities.Reservation;
+
 public class program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		System.out.print("Room number: ");
-		int number = sc.nextInt();
-		System.out.print("check-in date (dd/mm/yyy)");
-		Date checkin = sdf.parse(sc.next());
-		System.out.print("check-out date (dd/mm/yyy)");
-		Date checkout = sdf.parse(sc.next());
-		
-		
-		
+
+		try {
+			System.out.print("Room number: ");
+			int number = sc.nextInt();
+			System.out.print("check-in date (dd/mm/yyy)");
+			Date checkin = sdf.parse(sc.next());
+			System.out.print("check-out date (dd/mm/yyy)");
+			Date checkout = sdf.parse(sc.next());
+
+			Reservation reservation = new Reservation(number, checkin, checkout);
+			System.out.println("Reservation : " + reservation);
+			System.out.println();
+
+			System.out.println("Enter data to update the reservation: ");
+			System.out.print("check-in date (dd/mm/yyy)");
+			checkin = sdf.parse(sc.next());
+			System.out.print("check-out date (dd/mm/yyy)");
+			checkout = sdf.parse(sc.next());
+
+			reservation.updatedDates(checkin, checkout);
+			System.out.println("Reservation " + reservation);
+		} catch (ParseException e) {
+			System.out.println("Invalid date format");
+		} catch (IllegalArgumentException e) {
+			System.out.println("Error in reservation");
+
+		}
+
 		sc.close();
 	}
 
--- src/moldel/entities/Reservation.java
+++ src/moldel/entities/Reservation.java
@@ -8,7 +8,7 @@ public class Reservation {
 	private Integer roomnumber;
 	private Date checkIn;
 	private Date checkout;
-	private static SimpleDateFormat sdf =  new SimpleDateFormat("dd,MM,yyy");
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd,MM,yyy");
 
 	public Reservation(Integer roomnumber, Date checkIn, Date checkout) {
 
@@ -52,23 +52,25 @@ public class Reservation {
 	}
 
 	public void updatedDates(Date checkin, Date checkout) {
-
+		Date now = new Date();
+		if (checkin.before(now) || checkout.before(now)) {
+			throw new IllegalArgumentException(" Reservation dates for updates must be future dates");
+		}
+		if(!checkout.after(checkin)) {
+			throw new IllegalArgumentException("Checkout date must be after checkin date");
+			
+		}
+		
+		
 		this.checkIn = checkin;
 		this.checkout = checkout;
 
 	}
 
-	@Override 
+	@Override
 	public String toString() {
-		
-		return "Room " 
-		+ roomnumber 
-		+ ", checkin"
-		+ sdf.format(checkIn)
-		+", checkout "
-		+sdf.format(checkout)
-		+ ", "
-		+ duration()
-		+" nigths";
+
+		return "Room " + roomnumber + ", checkin" + sdf.format(checkIn) + ", checkout " + sdf.format(checkout) + ", "
+				+ duration() + " nigths";
 	}
 }
